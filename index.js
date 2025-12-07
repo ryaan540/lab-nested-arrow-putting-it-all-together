05683c15cb26295ec5499b1816c9341b20abf527
ryaan540! <rkinyua841@gmail.com>
2025-12-07 21:40:46 +0300

    completed lab-nested-arrow-putting-it-all-together

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
 
+  function attemptLogin(passwordAttempt)  {
+
+    attemptCount++;
+
+    
+    if (isLocked === true) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      isLocked = false;
+      return "Login successful";
+      }  else if(attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+
+  if (attemptCount >= 3) {
+    isLocked = true;
+    return "Account locked due to too many failed login attempts";
+  }
+  }
+
+  return attemptLogin;
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+   ...(typeof createLoginTracker !== 'undefined'&& { createLoginTracker })
 };
